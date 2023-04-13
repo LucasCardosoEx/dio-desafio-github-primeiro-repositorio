1f75b002258571c172b7a1fec149b1263b97ab26
Lucas Cardoso da Silva <lucascardoso.ds16@gmail.com>
2023-04-13 16:41:59 -0300

    Classes Mentoria e Curso

--- desafioDioPoo/desafioDioPoo/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.dio.desafio.desafiodiopoo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.dio.desafio.desafiodiopoo to javafx.fxml;
-    exports com.dio.desafio.desafiodiopoo;
-}
