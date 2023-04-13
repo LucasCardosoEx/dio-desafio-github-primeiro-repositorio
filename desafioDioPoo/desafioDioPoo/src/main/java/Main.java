5c698b7b87af555a1e400198bfa927c700e34c1d
Lucas Cardoso da Silva <lucascardoso.ds16@gmail.com>
2023-04-13 17:46:50 -0300

    Finalizando

--- desafioDioPoo/desafioDioPoo/src/main/java/Main.java
+++ desafioDioPoo/desafioDioPoo/src/main/java/Main.java
@@ -1,5 +1,4 @@
-import com.dio.desafio.desafiodiopoo.Curso;
-import com.dio.desafio.desafiodiopoo.Mentoria;
+import com.dio.desafio.desafiodiopoo.*;
 
 import java.time.LocalDate;
 
@@ -15,13 +14,46 @@ public class Main {
         curso2.setDescricao("Descrição do curso de JavaScript");
         curso2.setCargaHoraria(4);
 
+        // Exemplo de polimorfismo
+        // Conteudo conteudo = new Curso();
+
         Mentoria mentoria1 = new Mentoria();
         mentoria1.setTitulo("Mentoria de Java");
         mentoria1.setDescricao("Descrição Mentoria Java");
         mentoria1.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria1);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria1);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Dev");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+        Dev dev1 = new Dev();
+        dev1.setNome("Lucas");
+        dev1.inscreverBootcamp(bootcamp);
+        System.out.println("Counteúdos Inscritos Lucas " + dev1.getConteudosInscritos());
+        dev1.progredir();
+        dev1.progredir();
+        dev1.progredir();
+        System.out.println("Counteúdos Inscritos Lucas " + dev1.getConteudosInscritos());
+        System.out.println("Counteúdos Concluidos Lucas " + dev1.getConteudosConcluidos());
+        System.out.println("XP: " + dev1.calcularTotalXp());
+
+        Dev dev2 = new Dev();
+        dev2.setNome("Roberto Penno Spagolla");
+        dev2.inscreverBootcamp(bootcamp);
+        System.out.println("Counteúdos Inscritos Roberto " + dev2.getConteudosInscritos());
+        dev2.progredir();
+        dev2.progredir();
+        dev2.progredir();
+        System.out.println("Counteúdos Inscritos Roberto " + dev2.getConteudosInscritos());
+        System.out.println("Counteúdos Concluidos Roberto " + dev2.getConteudosConcluidos());
+        System.out.println("XP: " + dev2.calcularTotalXp());
+
     }
 }
--- desafioDioPoo/desafioDioPoo/src/main/java/com/dio/desafio/desafiodiopoo/Curso.java
+++ desafioDioPoo/desafioDioPoo/src/main/java/com/dio/desafio/desafiodiopoo/Curso.java
@@ -1,36 +1,22 @@
 package com.dio.desafio.desafiodiopoo;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
+public class Curso extends Conteudo {
     private int cargaHoraria;
+    @Override
+    public double calcularXp() {
+        return 0;
+    }
 
     @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", cargaHoraria=" + cargaHoraria +
                 '}';
     }
 
     public Curso() {
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
     }
 
     public int getCargaHoraria() {
@@ -40,4 +26,5 @@ public class Curso {
     public void setCargaHoraria(int cargaHoraria) {
         this.cargaHoraria = cargaHoraria;
     }
+
 }
